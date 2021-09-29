64d4242d4fbd5ef2554358c54e978b4dac4757d9
Fabio Dionisio <fabiogdionisio@gmail.com>
2021-09-29 13:34:00 -0300

    finish the 'querystring' test

--- querystring/src/App.js
+++ querystring/src/App.js
@@ -1,16 +1,33 @@
 import React, { Component } from 'react';
 
 class App extends Component {
+  // Usually when i'm faced with this kind of problem, I use the 'query-string' library
+  // that simplifies this implementation. In this example I chose to parse the querystring manually;
+  
   state = {
-    text: "Default text"
+    queryParams: {},
+  }
+
+  componentDidMount() {
+    const queryString = new URLSearchParams(this.props.location.search);
+    const temp = [];
+
+    queryString.forEach((value, key) => (
+      temp.push({ key, value })
+    ))
+
+    this.setState({ 
+      queryParams: temp.reduce((prev, curr) => ({ ...prev, [curr.key]: curr.value }), {})
+    })
   }
 
   render() {
-    const { text } = this.state;
+    const { queryParams } = this.state;
+
     return <div>
-      <div>
-        {text}
-      </div>
+      <ul>
+        {Object.entries(queryParams).map(([key, value]) => <li key={key}>{key}: {value}</li>)}
+      </ul>
     </div>;
   }
 }
