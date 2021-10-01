d98c4a18f3ec818e5674fb773826fab3e451a45a
Fabio Dionisio <fabiogdionisio@gmail.com>
2021-10-01 09:08:48 -0300

    hotfix validation test

--- validation-form/src/services/apiService.js
+++ validation-form/src/services/apiService.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: process.env.REACT_APP_API_BASEURL,
+  baseURL: process.env.REACT_APP_API_BASEURL || "https://toughland.com:9095/api",
 })
 
 export const postForm = (values) => {
